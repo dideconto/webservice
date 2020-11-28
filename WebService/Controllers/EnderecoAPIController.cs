aa4af2d90c3104e6db785188ba26a37b6a893c9e
Diogo Deconto <dideconto@gmail.com>
2020-11-28 14:28:14 -0300

    Criação da WebAPI

---
+++ WebService/Controllers/EnderecoAPIController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    [Route("api/Endereco")]
+    [ApiController]
+    public class EnderecoAPIController : ControllerBase
+    {
+        private List<Endereco> lista = new List<Endereco>
+        {
+            new Endereco { Id = 1, Cep = "68976-970", Logradouro = "Rua Laurita Almeida Barbosa 115", Localidade = "Itaubal"},
+            new Endereco { Id = 2, Cep = "87580-970", Logradouro = "Avenida Brasil 1813", Localidade = "Alto Piquiri"},
+            new Endereco { Id = 3, Cep = "78770-970", Logradouro = "Avenida Sete de Setembro 459", Localidade = "Alto Garças"}
+        };
+        //private List<Endereco> lista = new List<Endereco>();
+
+        //GET: /api/Endereco/Listar
+        [HttpGet]
+        [Route("Listar")]
+        public IActionResult Listar()
+        {
+            if (lista.Count == 0)
+            {
+                return BadRequest(new { msg = "Lista vazia" });
+            }
+            return Ok(lista);
+        }
+
+        //GET: /api/Endereco/Buscar/1
+        [HttpGet]
+        [Route("Buscar/{id}")]
+        public IActionResult Buscar(int id)
+        {
+            var endereco = lista.FirstOrDefault(x => x.Id == id);
+            if (endereco != null)
+            {
+                return Ok(endereco);
+            }
+            return NotFound(new { msg = "Endereço não encontrado!" });
+        }
+
+        //POST: /api/Endereco/Cadastrar
+        [HttpPost]
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(Endereco endereco)
+        {
+            endereco.CriadoEm = DateTime.Now;
+            lista.Add(endereco);
+            return Created("", lista);
+        }
+    }
+}
---
+++ WebService/Models/Endereco.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebService.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Localidade { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
